2662d074f795e3c3768ac63fc9d7011811cbd34c
arunkatta97 <arun.katta97@gmail.com>
2022-04-30 09:46:19 -0400

    changes in db

--- app/songs/__init__.py
+++ app/songs/__init__.py
@@ -31,7 +31,7 @@ def songs_browse(page):
 def songs_upload():
     form = csv_upload()
     if form.validate_on_submit():
-        log = logging.getLogger("myApp")
+        # log = logging.getLogger("mycsv")
 
         filename = secure_filename(form.file.data.filename)
         filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
@@ -41,10 +41,11 @@ def songs_upload():
         with open(filepath) as file:
             csv_file = csv.DictReader(file)
             for row in csv_file:
-                list_of_songs.append(Song(row['Name'],row['Artist']))
+                list_of_songs.append(Song(row['Name'],row['Artist'], row['Genre'], row['Year']))
 
         current_user.songs = list_of_songs
         db.session.commit()
+        # log.info(f"CSV file uploaded by {current_user}")
 
         return redirect(url_for('songs.songs_browse'))
 

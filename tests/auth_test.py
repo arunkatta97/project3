58febbc0a392061010858bebb9d2ce004aa34b70
arunkatta97 <arun.katta97@gmail.com>
2022-04-30 22:53:48 -0400

    csv test and auth test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,4 +1,8 @@
 """This test the homepage"""
+from app import db
+import logging
+from app.db.models import User, Song
+
 
 def test_request_main_menu_links(client):
     """This makes the index page"""
@@ -15,3 +19,22 @@ def test_auth_pages(client):
     assert response.status_code == 200
     response = client.get("/login")
     assert response.status_code == 200
+
+def test_registration(client):
+    """Test if a user logs in that it redirects to login page"""
+    with client:
+        res = client.post('/register', data=dict(email="ak298@gmail.com", password='testtest'), follow_redirects=True)
+        print(res.data)
+        assert res.status_code == 400
+
+
+def test_dashboard_access(client):
+    """Test dashboard access"""
+    res = client.get("/dashboard")
+    assert res.status_code == 302
+
+
+def test_authenticated_user(client):
+    """Test user authentication"""
+    user = User("ak298@njit.edu", "testtest")
+    assert user.is_authenticated() == True
